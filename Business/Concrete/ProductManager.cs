43dda6a09352db81a4756c10c83de91167f48ecb
Anıl Özrenk <anil.ozrenk@gmail.com>
2021-01-31 00:15:27 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entitites.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _iProductDal;
+
+        public ProductManager(IProductDal iProductDal)
+        {
+            _iProductDal = iProductDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _iProductDal.GetAll();
+        }
+    }
+}
