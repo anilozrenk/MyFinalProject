c8216bc3e846c0ab1c511d49cbf150ab4099d263
Anıl Özrenk <anil.ozrenk@gmail.com>
2021-02-04 00:10:39 +0300

    03.02.2021

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -9,16 +9,31 @@ namespace Business.Concrete
 {
     public class ProductManager : IProductService
     {
-        IProductDal _iProductDal;
+        IProductDal _productDal;
 
         public ProductManager(IProductDal iProductDal)
         {
-            _iProductDal = iProductDal;
+            _productDal = iProductDal;
         }
 
         public List<Product> GetAll()
         {
-            return _iProductDal.GetAll();
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p=>p.CategoryId==id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+        }
+
+        public List<Product> GetByUnitsPerStock(short min, short max)
+        {
+            return _productDal.GetAll(p => p.UnitsInStock >= min && p.UnitsInStock <= max);
         }
     }
 }
--- DataAccess/Abstract/ICategoryDal.cs
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -5,8 +5,8 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDal : IEntitiyRepository<Product>
+    public interface ICategoryDal:IEntitiyRepository<Category>
     {
-        
+       
     }
 }
