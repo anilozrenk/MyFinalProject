31879ea22298677ccf8cda1364cd0e6f156c442d
Anıl Özrenk <anil.ozrenk@gmail.com>
2021-03-11 17:46:53 +0300

    cors

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -28,7 +28,7 @@ namespace WebAPI.Controllers
             var result = _productService.GetAll();
             if (result.Success==true)
             {
-                return Ok(result.Data);
+                return Ok(result);
             }
             return BadRequest(result.Message);
         }
@@ -39,7 +39,7 @@ namespace WebAPI.Controllers
             var result = _productService.GetById(id);
             if (result.Success)
             {
-                return Ok(result.Data);
+                return Ok(result);
             }
             return BadRequest(result);
         }
